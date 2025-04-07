41fbc7477dc3fc48014267581076a18f8a292aee
simoneputeo <s.puteo@gmail.com>
2025-04-07 15:21:39 +0200

    correzione sintassi .toLowerCase()

--- 01-palindromi/script.js
+++ 01-palindromi/script.js
@@ -5,7 +5,7 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 // Input della parola e rimozione eventuali caratteri in maiuscolo
 // (che romperebbero la funzione)
-let word = prompt("Inserisci una parola").toLowerCase
+let word = prompt("Inserisci una parola").toLowerCase()
 
 //Applicazione funzione isPalindrome alla variabile Word
 function isPalindrome(word) {
@@ -26,8 +26,8 @@ function isPalindrome(word) {
 // Output del valore booleano
 if (isPalindrome(word)) {
     // Messaggio in caso di true
-    console.log("La parola che hai inserito e' palindroma");
+    alert("La parola che hai inserito e' palindroma");
 } else {
     // Messaggio in caso di false
-    console.log("La parola che hai inserito non e' palindroma");
+    alert("La parola che hai inserito non e' palindroma");
 }
